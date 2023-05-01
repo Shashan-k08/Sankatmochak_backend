6f187744db0ae0e4d97f9895e68b29bc38cda7d7
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-05-02 01:49:37 +0530

    database added

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI ="mongodb://localhost:27017/Sankaktmochak"
+const mongoURI ="mongodb://Shashan_k08:Shashanktiwari123456@ac-kqvvxsf-shard-00-00.ikfvhoz.mongodb.net:27017,ac-kqvvxsf-shard-00-01.ikfvhoz.mongodb.net:27017,ac-kqvvxsf-shard-00-02.ikfvhoz.mongodb.net:27017/Sankatmochak?ssl=true&replicaSet=atlas-i7ps5d-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 const connectToMongo =()=>{
     mongoose.connect(mongoURI)
